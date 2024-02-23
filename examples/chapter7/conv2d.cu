6ce4b23a9d6ae0345d7b798d8288ec4265997c47
drisspg <drisspguessous@gmail.com>
2024-02-22 20:23:37 -0800

    update conv1d

--- examples/chapter7/conv2d.cu
+++ examples/chapter7/conv2d.cu
@@ -1,6 +1,7 @@
 #include "src/include/utils.h"
+#include "src/include/tensors.h"
 
-#include <cmath>
+#include <cstddef>
 #include <fmt/core.h>
 #include <fmt/ranges.h>
 
@@ -12,65 +13,61 @@
 using namespace simple_cuda;
 using KernelFunc = void (*)(const float *, const float *, float *, int);
 
-using host_vec = thrust::host_vector<float>;
-using device_vec = thrust::device_vector<float>;
+using one_d  = Extent<1>;
 
-template <int tile_size, int filter_size>
+template <int tile_size, int filter_radius>
 __global__ void Conv1D(const float *input, const float *filter, float *output,
-                       const int input_size) {
+                       const int numel) {
   const int global_idx = blockIdx.x * blockDim.x + threadIdx.x;
-  if (global_idx >= input_size) {
+  if (global_idx >= numel) {
     // Early return for invalid output elements
     return;
   }
-  const int kernel_width = filter_size / 2;
+
   float accumulator = 0.0;
 #pragma unroll
-  for (int j{-kernel_width}; j <= kernel_width; j++) {
-    if (0 < global_idx + j < input_size) {
-      accumulator += input[global_idx + j] * filter[j + kernel_width];
+  for (int j{-filter_radius}; j <= filter_radius; j++) {
+    if (0 < global_idx + j < numel) {
+      accumulator += input[global_idx + j] * filter[j + filter_radius];
     }
   }
   // We early returned so we can write freely
   output[global_idx] += accumulator;
 }
 
-template <int tile_size, int filter_size>
+template <int tile_size, int filter_radius>
 __device__ void fill_tile(const float *input, float *input_tile,
-                          const int global_idx, const int input_size,
-                          const int kernel_width) {
+                          const int global_idx, const int input_size) {
   // Load the non ghost cells
-  input_tile[threadIdx.x + kernel_width] = global_idx < input_size? input[global_idx]: 0.0;
+  input_tile[threadIdx.x + filter_radius] = global_idx < input_size? input[global_idx]: 0.0;
 
   // Edges load in ghost cells
-  if (static_cast<int>(threadIdx.x - kernel_width) < 0) {
-    const int offset = global_idx - kernel_width;
+  if (static_cast<int>(threadIdx.x - filter_radius) < 0) {
+    const int offset = global_idx - filter_radius;
     input_tile[threadIdx.x] = offset >= 0 ? input[offset] : 0.0;
   }
-  if (threadIdx.x + kernel_width >= tile_size) {
-    const int offset = global_idx + kernel_width;
-    input_tile[kernel_width + threadIdx.x + kernel_width] =
+  if (threadIdx.x + filter_radius >= tile_size) {
+    const int offset = global_idx + filter_radius;
+    input_tile[filter_radius + threadIdx.x + filter_radius] =
         offset < input_size ? input[offset] : 0.0;
   }
 }
 
-template <int tile_size, int filter_size>
+template <int tile_size, int filter_radius>
 __global__ void Conv1D_shmem(const float *input, const float *filter,
                              float *output, const int input_size) {
   const int global_idx = blockIdx.x * blockDim.x + threadIdx.x;
-  const int kernel_width = filter_size / 2;
   // We stored the the tile_size which is equivelant to block size + the filter
   // size for the left and right boundaries
-  __shared__ float input_tile[tile_size + filter_size];
+  __shared__ float input_tile[tile_size + (2 * filter_radius)];
 
-  fill_tile<tile_size, filter_size>(input, input_tile, global_idx, input_size,
-                                    kernel_width);
+  fill_tile<tile_size, filter_radius>(input, input_tile, global_idx, input_size);
   __syncthreads();
   float accumulator = 0.0;
 #pragma unroll
-  for (int j{-kernel_width}; j <= kernel_width; j++) {
-    const int tile_idx = kernel_width + threadIdx.x + j;
-    accumulator += input_tile[tile_idx] * filter[j + kernel_width];
+  for (int j{-filter_radius}; j <= filter_radius; j++) {
+    const int tile_idx = filter_radius + threadIdx.x + j;
+    accumulator += input_tile[tile_idx] * filter[j + filter_radius];
   }
 
   if (global_idx < input_size) {
@@ -96,46 +93,42 @@ template <typename T> T cpp_kernel(T const &input, T const &filter) {
   return output;
 }
 
-void Test(KernelFunc func, const int vec_length, const int filter_size,
+void Test(KernelFunc func, const size_t n_rows, const size_t n_cols, const size_t filter_radius,
           dim3 grid, dim3 block) {
-  host_vec input_vec(vec_length);
-  host_vec output_vec(vec_length);
-  host_vec filter(filter_size);
+  one_d tensor_extents({n_rows});
+  one_d filter_extents({2*filter_radius});
 
-  // fill_random(input_vec);
-  // fill_random(filter);
+  HostTensor<float, one_d> input_vec(tensor_extents);
+  HostTensor<float, one_d> output_vec(tensor_extents);
+  HostTensor<float, one_d> filter(filter_extents);
 
-  std::fill(input_vec.begin(), input_vec.end(), 1);
-  std::fill(output_vec.begin(), output_vec.end(), 0);
-  std::fill(filter.begin(), filter.end(), 2);
+  std::fill(input_vec.data_.begin(), input_vec.data_.end(), 1);
+  std::fill(output_vec.data_.begin(), output_vec.data_.end(), 0);
+  std::fill(filter.data_.begin(), filter.data_.end(), 2);
 
-  device_vec input_vec_d(input_vec);
-  device_vec output_vec_d(output_vec);
-  device_vec filter_d(filter);
+  auto input_vec_d = input_vec.to_device();
+  auto output_vec_d = output_vec.to_device();
+  auto filter_d = filter.to_device();
 
-  float *input_vec_ptr = thrust::raw_pointer_cast(input_vec_d.data());
-  float *output_vec_ptr = thrust::raw_pointer_cast(output_vec_d.data());
-  float *filter_ptr = thrust::raw_pointer_cast(filter_d.data());
-
-  func<<<grid, block>>>(input_vec_ptr, filter_ptr, output_vec_ptr, vec_length);
+  func<<<grid, block>>>(input_vec_d.data_ptr(), filter_d.data_ptr(), output_vec_d.data_ptr(), tensor_extents.numel());
   cudaCheckErrors("kernel launch failure");
   cudaDeviceSynchronize();
 
-  auto host_output = host_vec(output_vec_d);
-  float *host_output_ptr = thrust::raw_pointer_cast(host_output.data());
+  auto host_output = output_vec_d.to_host();
+  auto host_output_ptr = host_output.data_ptr();
 
-  const auto cpp_anwser = cpp_kernel(host_vec(input_vec), host_vec(filter));
+  const auto cpp_anwser = cpp_kernel(input_vec.data_, filter.data_);
 
-  for (const int idx : std::views::iota(0, vec_length)) {
+  for (const int idx : std::views::iota(0, int(tensor_extents.numel()))) {
     if (host_output_ptr[idx] != cpp_anwser[idx]) {
       std::string error_string = "Houston we have a problem!\n";
       error_string += fmt::format("At ({}) found value: {} instead of {}!\n",
                                   idx, host_output_ptr[idx], cpp_anwser[idx]);
       std::cout << error_string;
 
-      if (vec_length < 32) {
+      if (tensor_extents.numel() <= 32) {
         fmt::print("Good:{}\n", fmt::join(cpp_anwser, ", "));
-        fmt::print("Bad:{}\n", fmt::join(host_output, ", "));
+        fmt::print("Bad:{}\n", fmt::join(host_output.data_, ", "));
       }
 
       exit(1);
@@ -146,18 +139,18 @@ void Test(KernelFunc func, const int vec_length, const int filter_size,
 
 int main() {
   // Standard Matmul
-  constexpr int max_length = 1000000;
-  constexpr int filter_length = 21;
+  constexpr int max_length = 10000;
+  constexpr int filter_radius = 21;
   constexpr int block_size = 1024;
 
   // dimx is inner dim, dimy is outerdim
   dim3 grid(ceil_div(max_length, block_size));
   dim3 block(block_size);
 
-  Test(Conv1D<block_size, filter_length>, max_length, filter_length, grid,
+  Test(Conv1D<block_size, filter_radius>, max_length, max_length, filter_radius, grid,
        block);
 
-  Test(Conv1D_shmem<block_size, filter_length>, max_length, filter_length, grid,
+  Test(Conv1D_shmem<block_size, filter_radius>, max_length, max_length, filter_radius, grid,
        block);
 
   // profile the relevant kernels:
